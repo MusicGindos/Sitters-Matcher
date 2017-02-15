3fce8791ccb10ca2826dec0b4812af74ce08ec17
Arel Gindos <rlgindos@gmail.com>
2017-02-15 20:35:37 +0200

    Added priority to filter and finished matcher

--- matcher/index.js
+++ matcher/index.js
@@ -1,12 +1,8 @@
-/**
- * Created by Arel on 14/02/2017.
- */
 'use strict';
 
 var express		    = require('express'),
     fs			    = require('fs'),
     _               = require('lodash'),
-    DEBUG           = true,
     localJSONPath   = "data/data.json",
     jsonfile        = require('jsonfile'),
     googleDistance  = require('google-distance'),
@@ -16,42 +12,20 @@ var express		    = require('express'),
     origin          = null,
     destination     = null,
     distance        = null,
-    score           = null,
+    matchScore      = null,
     generalScore    = 0,
     locationScore   = null,
     experienceScore = null,
     hobbiesScore    = null,
     knowledgeScore  = null,
-    result          = null,
-    bonusScore      = 5;
+    bonusScore      = 2.5,
+    score           = 0,
+    divide          = 0,
+    tempScore       = 0;
 
-
-var error = function(next, msg, status){
-    var err = new Error();
-    err.status = status;
-    err.message = msg;
-    //next(err);
-};
-
-var localJson = function(path){
-    var obj;
-    fs.readFile(path, function(err, json){
-        if (err) {
-            console.log(err.message);
-            //error(next, err.message, 500);
-        }
-        else{
-            obj =  JSON.parse(json);
-            return obj;
-        }
-
-    });
-
-};
-
-var computeSync = function(origin,destination,callback){
-    googleDistance.apiKey = "AIzaSyBwP7ZYyCO86H41nE-E5eHYPCDir9yBpc0";
-    googleDistance.get(
+var computeSync = function(origin,destination,callback){  // google-distance is async and we need it to be sync so we use wrapper with flag
+    googleDistance.apiKey = "AIzaSyBwP7ZYyCO86H41nE-E5eHYPCDir9yBpc0";  // google-distance apikey for make more calls
+    googleDistance.get(  // compute distance between 2 locations, can be street-houseNumber-city OR latitude/longitude
         {
             origin: origin,
             destination: destination
@@ -62,50 +36,55 @@ var computeSync = function(origin,destination,callback){
                 return;
             }
             maindata = data;
-            finish = false;
+            finish = false; // exit the sync loop
             callback(data);
         });
 };
 
-var computeDistance = function(origin,destination) {
+var computeDistance = function(origin,destination) { // make compute distance sync with flag and while loop
     computeSync(origin,destination, function(result){
-
         maindata = result;
     });
     while(finish) {
         require('deasync').sleep(100); // sync for google-distance api
     }
-    finish = true;
+    finish = true; // for next sync call
     return maindata;
 };
 
-var syncFunc = function(parent,sitter,filter,distance) {
+var computeMatchScore = function(parent,sitter,filter,distance) {  // make compute score sync with flag and while loop
     computeScore(parent,sitter,filter,distance, function(result){
         maindata = result;
     });
     while(finish) {
         require('deasync').sleep(100);
     }
-    finish = true;
+    finish = true; // for next sync call
     return maindata;
 };
 
-var computeScore = function(parent,sitter,filter,distance,callback){
-    //TODO: take care of filter
-    if(parent.children.age < sitter.minAge || parent.children.age > sitter.maxAge || sitter.hourFee > parent.maxPrice)
-        return 0;
+var computeScore = function(parent,sitter,filter,distance,callback){ // compute match score between sitter-parent-child
+    if(parent.children.age < sitter.minAge || parent.children.age > sitter.maxAge || sitter.hourFee > parent.maxPrice){
+        finish = false;
+        callback(0);
+        return;
+    }
     if(parent.children.specialNeeds.length != 0){
         for (var i = 0, len = parent.children.specialNeeds.length; i < len; i++) {
-            if (_.indexOf(sitter.specialNeeds, parent.children.specialNeeds[i]) == -1)
-                return 0;  // sitter don't have the speicalNeed that the child need and the match score is 0
+            if (_.indexOf(sitter.specialNeeds, parent.children.specialNeeds[i]) == -1){
+                finish = false;
+                callback(0);
+                return;
+            }
+                  // sitter don't have the speicalNeed that the child need and the match score is 0
         }
     }
     distance = distance.distance.split(' ');
-    distance = Number(distance[0]);
-    if(distance <= 5)   // calculate location score, for each added 1 km more than 5, reduce 2
+    distance = Number(distance[0]);  // distance between 2 locations with google-distance
+    if(distance <= 5)   // calculate location score
         locationScore = 100;
     else{
-        locationScore = 100 - ((distance-5) * 2);
+        locationScore = 100 - ((distance-5) * 2); //for each added 1 km more than 5, reduce 2
     }
     if(locationScore < 0)  // if the sitter is more than 50 kilometer from thr child address
         locationScore = 0;
@@ -120,35 +99,32 @@ var computeScore = function(parent,sitter,filter,distance,callback){
         experienceScore = 60;
     else if(sitter.experience >= 0)
         experienceScore = 50;
-    else
-        experienceScore = 0;
 
-    if(parent.children.hobbies.length > 0){
+    if(parent.children.hobbies.length > 0){  // calculate hobbies match of the sitter with child
         _.forEach(parent.children.hobbies, function(hobbie) {
-            if(_.indexOf(sitter.hobbies,hobbie) > -1){ // check if sitter have the hobbies
-                hobbiesScore +=  (100 / parent.children.hobbies.length);
+            if(_.indexOf(sitter.hobbies,hobbie) > -1){ // check if sitter have the hobbie
+                hobbiesScore +=  (100 / parent.children.hobbies.length);  // add the fraction of 1 match from the length of the hobbie array
             }
         });
     }
     else{
-        hobbiesScore = -1;
+        hobbiesScore = -1;  // children don't have any hobbies
     }
 
     if(parent.children.expertise.length > 0){
         _.forEach(parent.children.expertise, function(exp) {
             if(_.indexOf(sitter.expertise,exp) > -1){ // check if sitter have the expertise
-                knowledgeScore +=  (100 / parent.children.expertise.length);
+                knowledgeScore +=  (100 / parent.children.expertise.length);   // add the fraction of 1 match from the length of the expertise array
             }
         });
     }
     else{
-        knowledgeScore = -1;
+        knowledgeScore = -1;  // children don't have any expertise
     }
 
-
     if(filter == null){
-        var divide = 2;
-        var score = locationScore + experienceScore;
+        divide = 2;
+        score = locationScore + experienceScore;
         if(knowledgeScore >= 0 ){
             divide++;
             score += knowledgeScore;
@@ -159,10 +135,31 @@ var computeScore = function(parent,sitter,filter,distance,callback){
         }
         generalScore = score/divide;
     }
-    else{  // if user want to give priority to category
-        //TODO: implement filter score
+    else{  // 70% for the filter category, else get 10% or more if child don't have the categories
+        divide = 1;
+        tempScore = 0;
+        //score = locationScore + experienceScore;
+        if(knowledgeScore >= 0 ){
+            divide++;
+            tempScore += knowledgeScore;
+        }
+        if(hobbiesScore >= 0){
+            divide++;
+            tempScore += hobbiesScore;
+        }
+
+        if(filter == "location"){
+            score += locationScore * 0.7;
+            tempScore += experienceScore;
+        }
+        else if(filter == "experience"){
+            score += experienceScore * 0.7;
+            tempScore += locationScore;
+        }
+        generalScore += (0.3 * (tempScore / divide)) + score;
     }
 
+    // bonus section - for each bonus sitter gets +5% match to general score
     if(sitter.availableNow)
         generalScore += bonusScore;
     if(sitter.mobility)
@@ -174,12 +171,9 @@ var computeScore = function(parent,sitter,filter,distance,callback){
 
     if(generalScore > 100) // more than 100% match with the bonuses
         generalScore = 100;
-
-    maindata = data;
-    finish = false;
+    finish = false;  // exit the sync loop
+    generalScore = ((generalScore * 0.7) + (sitter.personalityScore * 0.3)); // matcher = 70%, personality-test = 30%
     callback(generalScore);
-
-
 };
 
 exports.getMatchScore = function(){
@@ -187,11 +181,11 @@ exports.getMatchScore = function(){
     origin = data.parent.address.street + ' ' + data.parent.address.houseNumber + ' ' + data.parent.address.city;
     destination = data.sitter.address.street + ' ' + data.sitter.address.houseNumber + ' ' + data.sitter.address.city;
     distance = computeDistance(origin,destination);
-    score = syncFunc(data.parent,data.sitter,null,distance);
-    if(score == 0){
-        return {"match_score":0};
+    matchScore = computeMatchScore(data.parent,data.sitter,'location',distance);
+    if(matchScore == 0){
+        return {"match_score":0};  // no match
     }
     else{
-        return {"match_score":Math.ceil(score)};
+        return {"match_score":Math.ceil(matchScore)};
     }
 };
